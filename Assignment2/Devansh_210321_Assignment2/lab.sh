1b8954954461a68cc05b2c441d563ecb7d2df66f
devansh-jain-2002 <d.jain.182002@gmail.com>
2022-06-12 18:22:46 +0530

    found a mistake in my code

--- Assignment2/Devansh_210321_Assignment2/lab.sh
+++ Assignment2/Devansh_210321_Assignment2/lab.sh
@@ -2,7 +2,7 @@
 bar(){
 	awk -F ',' '{print $1" "$2" "$3" "$5" "$6" "$7" "$10" "$11}' $1 > $2
 	awk -F ',' -f aux.awk  $1 >> $2
-	awk -F ',' '{if(NR>1) print $NF $1}' $1 | sort -rnk1 | head -5 >> $2
+	awk -F ',' '{if(NR>1) printf("%d %s\n",$NF,$1)}' $1 | sort -rnk1 | head -5 | sed 's/[^ ]* //' >> $2
 }
 foo(){
 	if [ -f "$1" ];
